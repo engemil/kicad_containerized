a974ab3f37e2854ba3523a09966534c4c0402b6f
Engemil <eng.emil.jenssen@gmail.com>
2025-05-14 00:57:00 +0200

    - added a testpath for kicad config - Merged setup script into start script

--- container/start.sh
+++ container/start.sh
@@ -13,4 +13,29 @@
 
 ##################################################
 
-docker compose -f ./docker-compose.yml up -d
+# Function to check if running as root
+check_sudo() {
+    if [ "$(id -u)" -eq 0 ]; then
+        return 0  # Running as root
+    else
+        return 1  # Not running as root
+    fi
+}
+
+# The command(s)
+COMMAND1="xhost -local:docker"
+COMMAND2="xhost +local:docker"
+COMMAND3="docker compose -f ./docker-compose.yml up -d"
+
+if check_sudo; then
+    # Running as root, execute command
+    $COMMAND1
+    $COMMAND2
+    $COMMAND3
+else
+    # Not running as root, use sudo for comannd
+    sudo $COMMAND1
+    sudo $COMMAND2
+    sudo $COMMAND3
+fi
+
